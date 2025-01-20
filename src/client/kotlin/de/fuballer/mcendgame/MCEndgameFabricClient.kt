69b9122e1335e0650bed1e50c28f66cdc6f66d43
maucon <55910883+maucon@users.noreply.github.com>
2025-01-20 21:11:04 +0100

    Initial setup (#1)
    * renaming
    add dependency
    
    * minor changes
    
    * rename gradle var
    
    * update build.yml

--- src/client/kotlin/de/fuballer/mcendgame/MCEndgameFabricClient.kt
+++
@@ -1,9 +0,0 @@
-package de.fuballer.mcendgame
-
-import net.fabricmc.api.ClientModInitializer
-
-object MCEndgameFabricClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
