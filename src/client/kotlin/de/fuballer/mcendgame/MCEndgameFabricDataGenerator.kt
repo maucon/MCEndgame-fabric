ce9ae2d918b9c85e8c9bd1d9eb83232b509db8be
maucon <maucon13@gmail.com>
2025-01-20 20:21:18 +0100

    Initial commit

---
+++ src/client/kotlin/de/fuballer/mcendgame/MCEndgameFabricDataGenerator.kt
@@ -0,0 +1,9 @@
+package de.fuballer.mcendgame
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object MCEndgameFabricDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
