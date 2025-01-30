508a772e46d5cd22d618657ecad9c32b32df1998
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2025-01-31 00:35:30 +0100

    add druids armor set

---
+++ src/main/kotlin/de/fuballer/mcendgame/components/item/custom/armor/helmet/iceborne/DruidsArmorMaterial.kt
@@ -0,0 +1,28 @@
+package de.fuballer.mcendgame.components.item.custom.armor.helmet.iceborne
+
+import de.fuballer.mcendgame.components.item.custom.armor.CustomArmorMaterial
+import de.fuballer.mcendgame.util.RegistryKeyUtil
+import net.minecraft.item.equipment.ArmorMaterial
+import net.minecraft.item.equipment.EquipmentAsset
+import net.minecraft.item.equipment.EquipmentType
+import net.minecraft.registry.RegistryKey
+import net.minecraft.registry.tag.ItemTags
+import net.minecraft.sound.SoundEvents
+
+object DruidsArmorMaterial : CustomArmorMaterial {
+    override val baseDurability = 15
+    override val registryKey: RegistryKey<EquipmentAsset> = RegistryKeyUtil.createEquipmentAssetKey("druids")
+
+    override val instance = ArmorMaterial(
+        baseDurability,
+        mapOf(
+            EquipmentType.CHESTPLATE to 7,
+        ),
+        15,
+        SoundEvents.ITEM_ARMOR_EQUIP_GENERIC,
+        3.0f,
+        0.1f,
+        ItemTags.REPAIRS_NETHERITE_ARMOR,
+        registryKey
+    )
+}
