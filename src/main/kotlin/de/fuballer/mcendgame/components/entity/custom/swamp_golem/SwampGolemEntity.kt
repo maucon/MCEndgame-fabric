6c5573fe08e2e706be50b04b7d7844348e794157
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2025-02-07 22:09:36 +0100

    add slam goal

--- src/main/kotlin/de/fuballer/mcendgame/components/entity/custom/swamp_golem/SwampGolemEntity.kt
+++ src/main/kotlin/de/fuballer/mcendgame/components/entity/custom/swamp_golem/SwampGolemEntity.kt
@@ -1,11 +1,15 @@
 package de.fuballer.mcendgame.components.entity.custom.swamp_golem
 
 import net.minecraft.entity.EntityType
-import net.minecraft.entity.ai.goal.*
+import net.minecraft.entity.ai.goal.ActiveTargetGoal
+import net.minecraft.entity.ai.goal.LookAroundGoal
+import net.minecraft.entity.ai.goal.LookAtEntityGoal
+import net.minecraft.entity.ai.goal.WanderAroundFarGoal
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.entity.mob.HostileEntity
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.server.world.ServerWorld
 import net.minecraft.world.World
 
 class SwampGolemEntity(
@@ -14,7 +18,8 @@ class SwampGolemEntity(
 ) : HostileEntity(type, world) {
 
     override fun initGoals() {
-        goalSelector.add(2, MeleeAttackGoal(this, 1.0, false))
+        //goalSelector.add(2, MeleeAttackGoal(this, 1.0, false))
+        goalSelector.add(2, SwampGolemSlamGoal(this, 1.0, false))
         goalSelector.add(7, WanderAroundFarGoal(this, 1.0))
         goalSelector.add(8, LookAtEntityGoal(this, PlayerEntity::class.java, 8.0f))
         goalSelector.add(8, LookAroundGoal(this))
@@ -30,5 +35,13 @@ class SwampGolemEntity(
                 .add(EntityAttributes.ATTACK_DAMAGE, 4.0)
                 .add(EntityAttributes.ARMOR, 5.0)
         }
+    }
+
+    fun slam() {
+        println("SLAM ANIMATION")
+    }
+
+    fun slamAttack(world: ServerWorld) {
+
     }
 }
---
+++ src/main/kotlin/de/fuballer/mcendgame/components/entity/custom/swamp_golem/SwampGolemSlamGoal.kt
@@ -0,0 +1,129 @@
+package de.fuballer.mcendgame.components.entity.custom.swamp_golem
+
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.ai.goal.Goal
+import net.minecraft.entity.ai.pathing.Path
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.predicate.entity.EntityPredicates
+import java.util.*
+import kotlin.math.max
+
+class SwampGolemSlamGoal(
+    private val mob: SwampGolemEntity,
+    private val speed: Double,
+    private val pauseWhenMobIdle: Boolean,
+) : Goal() {
+    private var path: Path? = null
+    private var targetX = 0.0
+    private var targetY = 0.0
+    private var targetZ = 0.0
+    private var updateCountdownTicks = 0
+    private var cooldown = 0
+    private var lastUpdateTime = 0L
+
+    init {
+        setControls(EnumSet.of(Control.MOVE, Control.LOOK))
+    }
+
+    override fun canStart(): Boolean {
+        val time = mob.world.time
+        if (time - lastUpdateTime < 20) return false
+        lastUpdateTime = time
+
+        val target = mob.target ?: return false
+        if (!target.isAlive) return false
+
+        path = mob.navigation.findPathTo(target, 0)
+        return path != null || mob.isInAttackRange(target)
+    }
+
+    override fun shouldContinue(): Boolean {
+        val target = mob.target ?: return false
+        if (!target.isAlive) return false
+
+        if (!pauseWhenMobIdle) return !mob.navigation.isIdle
+
+        if (!mob.isInWalkTargetRange(target.blockPos)) return false
+        return target !is PlayerEntity || (!target.isSpectator && !target.isCreative)
+    }
+
+    override fun start() {
+        mob.navigation.startMovingAlong(path, speed)
+        mob.isAttacking = true
+        updateCountdownTicks = 0
+        cooldown = 0
+    }
+
+    override fun stop() {
+        val target = mob.target
+        if (!EntityPredicates.EXCEPT_CREATIVE_OR_SPECTATOR.test(target)) {
+            mob.target = null
+        }
+
+        mob.isAttacking = false
+        mob.navigation.stop()
+    }
+
+    override fun shouldRunEveryTick() = true
+
+    override fun tick() {
+        val target = mob.target ?: return
+        update(target)
+        trySlam(target)
+    }
+
+    private fun shouldUpdate(
+        target: LivingEntity
+    ): Boolean {
+        if (updateCountdownTicks > 0) return false
+        if (!mob.visibilityCache.canSee(target) && !pauseWhenMobIdle) return false
+
+        if (targetX == 0.0 && targetY == 0.0 && targetZ == 0.0) return true
+        if (target.squaredDistanceTo(targetX, targetY, targetZ) >= 1.0) return true
+
+        return mob.random.nextFloat() < 0.05
+    }
+
+    private fun update(
+        target: LivingEntity
+    ) {
+        mob.lookControl.lookAt(target, 30.0f, 30.0f)
+
+        cooldown = max(cooldown - 1, 0)
+        updateCountdownTicks = max(updateCountdownTicks - 1, 0)
+
+        if (!shouldUpdate(target)) return
+
+        targetX = target.x
+        targetY = target.y
+        targetZ = target.z
+
+        updateCountdownTicks = 4 + mob.random.nextInt(7)
+
+        val distance = mob.squaredDistanceTo(target)
+        updateCountdownTicks += (distance / 100).toInt()
+
+        if (!mob.navigation.startMovingTo(target, speed)) {
+            updateCountdownTicks += 15
+        }
+
+        updateCountdownTicks = getTickCount(updateCountdownTicks)
+    }
+
+    private fun trySlam(target: LivingEntity) {
+        if (!canSlam(target)) return
+
+        resetCooldown()
+        mob.slam()
+        mob.slamAttack(getServerWorld(this.mob))
+    }
+
+    private fun resetCooldown() {
+        cooldown = getTickCount(50)
+    }
+
+    private fun isCooledDown() = cooldown <= 0
+
+    private fun canSlam(target: LivingEntity) =
+        isCooledDown() && mob.isInAttackRange(target) && mob.visibilityCache.canSee(target)
+}
