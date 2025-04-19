002654196897e15ca77aad7c48b27936ee6f7b22
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2025-04-19 16:01:18 +0200

    add filter inventory

--- src/main/kotlin/de/fuballer/mcendgame/components/item_filter/ItemFilterCommandRegisterer.kt
+++ src/main/kotlin/de/fuballer/mcendgame/components/item_filter/ItemFilterCommandRegisterer.kt
@@ -4,14 +4,12 @@ import com.mojang.brigadier.Command
 import de.maucon.mauconframework.di.annotation.Injectable
 import de.maucon.mauconframework.initializer.Initializer
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
-import net.minecraft.screen.GenericContainerScreenHandler
-import net.minecraft.screen.SimpleNamedScreenHandlerFactory
 import net.minecraft.server.command.CommandManager
-import net.minecraft.server.network.ServerPlayerEntity
-import net.minecraft.text.Text
 
 @Injectable
-class ItemFilterCommandRegisterer {
+class ItemFilterCommandRegisterer(
+    private val itemFilterService: ItemFilterService,
+) {
     @Initializer
     fun init() {
         CommandRegistrationCallback.EVENT.register(CommandRegistrationCallback { dispatcher, _, _ ->
@@ -19,18 +17,9 @@ class ItemFilterCommandRegisterer {
                 CommandManager.literal("filter")
                     .executes { context ->
                         val player = context.source.player ?: return@executes Command.SINGLE_SUCCESS
-                        openFilterInventory(player)
+                        itemFilterService.openFilterInventory(player)
                         Command.SINGLE_SUCCESS
                     })
         })
-    }
-
-    private fun openFilterInventory(player: ServerPlayerEntity) {
-        val title = Text.translatable("container.mcendgame.filter.title")
-        val screenHandlerFactory = SimpleNamedScreenHandlerFactory({ syncId, pInv, _ ->
-            GenericContainerScreenHandler.createGeneric9x1(syncId, pInv)
-        }, title)
-
-        player.openHandledScreen(screenHandlerFactory)
     }
 }
