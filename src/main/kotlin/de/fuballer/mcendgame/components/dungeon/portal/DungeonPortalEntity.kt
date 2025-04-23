1c0d6c25c53865e6ffe6fef3f23fc858bd88bb4f
maucon <maucon13@gmail.com>
2025-04-23 22:17:36 +0200

    cleanup

--- src/main/kotlin/de/fuballer/mcendgame/components/dungeon/portal/DungeonPortalEntity.kt
+++ src/main/kotlin/de/fuballer/mcendgame/components/dungeon/portal/DungeonPortalEntity.kt
@@ -4,7 +4,6 @@ import de.fuballer.mcendgame.components.portal.PortalEntity
 import de.fuballer.mcendgame.components.portal.teleport.TeleportLocation
 import de.maucon.mauconframework.stereotype.Entity
 import net.minecraft.server.world.ServerWorld
-import net.minecraft.world.World
 
 class DungeonPortalEntity(
     override var id: Int,
--- src/main/kotlin/de/fuballer/mcendgame/components/dungeon/portal/DungeonPortalRepository.kt
+++ src/main/kotlin/de/fuballer/mcendgame/components/dungeon/portal/DungeonPortalRepository.kt
@@ -3,7 +3,6 @@ package de.fuballer.mcendgame.components.dungeon.portal
 import de.maucon.mauconframework.di.annotation.Injectable
 import de.maucon.mauconframework.stereotype.extension.InMemoryMapRepository
 import net.minecraft.server.world.ServerWorld
-import net.minecraft.world.World
 
 @Injectable
 class DungeonPortalRepository : InMemoryMapRepository<Int, DungeonPortalEntity>() {
