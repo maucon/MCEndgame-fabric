0a957512db4ef5620947e4b8e865f50055568bff
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2025-07-05 19:03:42 +0200

    move service function into command

--- src/main/kotlin/de/fuballer/mcendgame/main/component/item/custom/command/GiveUniqueItemCommand.kt
+++ src/main/kotlin/de/fuballer/mcendgame/main/component/item/custom/command/GiveUniqueItemCommand.kt
@@ -1,19 +1,22 @@
 package de.fuballer.mcendgame.main.component.item.custom.command
 
+import com.mojang.brigadier.Command
 import com.mojang.brigadier.arguments.StringArgumentType
+import com.mojang.brigadier.context.CommandContext
+import de.fuballer.mcendgame.main.component.item.custom.armor.interfaces.UniqueAttributesItem
 import de.maucon.mauconframework.di.annotation.Injectable
 import de.maucon.mauconframework.initializer.Initializer
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
+import net.minecraft.item.Item
 import net.minecraft.server.command.CommandManager
+import net.minecraft.server.command.ServerCommandSource
 
 @Injectable
-class GiveUniqueItemCommand(
-    private val giveUniqueItemService: GiveUniqueItemService,
-) {
+class GiveUniqueItemCommand {
     companion object {
         private const val NAME = "give-unique"
-        const val UNIQUE_ITEM_ARGUMENT = "unique-item"
-        const val DOUBLE_ROLLS_ARGUMENT = "double-roll"
+        private const val UNIQUE_ITEM_ARGUMENT = "unique-item"
+        private const val DOUBLE_ROLLS_ARGUMENT = "rolls"
     }
 
     @Initializer
@@ -23,12 +26,26 @@ class GiveUniqueItemCommand(
                 .then(
                     CommandManager.argument(UNIQUE_ITEM_ARGUMENT, UniqueItemArgumentType())
                         .suggests(UniqueItemSuggestionProvider())
-                        .executes { giveUniqueItemService.giveUniqueItem(it, false) }
+                        .executes { giveUniqueItem(it, false) }
                         .then(
                             CommandManager.argument(DOUBLE_ROLLS_ARGUMENT, StringArgumentType.greedyString())
-                                .executes { giveUniqueItemService.giveUniqueItem(it, true) }
+                                .executes { giveUniqueItem(it, true) }
                         )
                 )
         )
     })
+
+    private fun giveUniqueItem(
+        context: CommandContext<ServerCommandSource>,
+        hasRolls: Boolean
+    ): Int {
+        val player = context.source.player ?: return 0
+        val uniqueItem = context.getArgument(UNIQUE_ITEM_ARGUMENT, Item::class.java) as? UniqueAttributesItem ?: return 0
+        val rolls = if (!hasRolls) listOf()
+        else context.getArgument(DOUBLE_ROLLS_ARGUMENT, String::class.java).split(" ").mapNotNull { it.toDoubleOrNull() }
+
+        player.giveItemStack(uniqueItem.getRolledStack(rolls))
+
+        return Command.SINGLE_SUCCESS
+    }
 }
