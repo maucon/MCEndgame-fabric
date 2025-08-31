bb6b811155b371ca2a6b2fddb9aefb32df303d7a
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2025-08-31 16:47:59 +0200

    make elytra rendering affected ghostly

---
+++ src/client/java/de/fuballer/mcendgame/client/mixin/renderer/ElytraFeatureRendererMixin.java
@@ -0,0 +1,79 @@
+package de.fuballer.mcendgame.client.mixin.renderer;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import de.fuballer.mcendgame.client.component.item.custom.armor.geistergaloschen.GhostlyVertexConsumer;
+import de.fuballer.mcendgame.client.util.EntityRenderStateMixinExtension;
+import de.fuballer.mcendgame.client.util.EquipmentRendererMixinExtension;
+import net.minecraft.client.model.Model;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.equipment.EquipmentModel;
+import net.minecraft.client.render.entity.equipment.EquipmentRenderer;
+import net.minecraft.client.render.entity.feature.ElytraFeatureRenderer;
+import net.minecraft.client.render.entity.model.ElytraEntityModel;
+import net.minecraft.client.render.entity.state.BipedEntityRenderState;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.component.type.EquippableComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.equipment.EquipmentAsset;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ElytraFeatureRenderer.class)
+public class ElytraFeatureRendererMixin<S extends BipedEntityRenderState> {
+    @Shadow
+    @Final
+    private EquipmentRenderer equipmentRenderer;
+
+    @Redirect(
+            method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/client/render/entity/state/BipedEntityRenderState;FF)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/equipment/EquipmentRenderer;render(Lnet/minecraft/client/render/entity/equipment/EquipmentModel$LayerType;Lnet/minecraft/registry/RegistryKey;Lnet/minecraft/client/model/Model;Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/util/Identifier;)V")
+    )
+    void render(
+            EquipmentRenderer instance,
+            EquipmentModel.LayerType layerType,
+            RegistryKey<EquipmentAsset> assetKey,
+            Model model,
+            ItemStack stack,
+            MatrixStack matrixStack,
+            VertexConsumerProvider vertexConsumerProvider,
+            int light,
+            @Nullable Identifier texture,
+            @Local(argsOnly = true) S bipedEntityRenderState,
+            @Local EquippableComponent equippableComponent,
+            @Local ElytraEntityModel elytraEntityModel,
+            @Local ItemStack itemStack,
+            @Local Identifier identifier
+    ) {
+        if (!EntityRenderStateMixinExtension.INSTANCE.isGhostly(bipedEntityRenderState)) {
+            equipmentRenderer.render(
+                    EquipmentModel.LayerType.WINGS,
+                    equippableComponent.assetId().get(),
+                    elytraEntityModel,
+                    itemStack,
+                    matrixStack,
+                    vertexConsumerProvider,
+                    light,
+                    identifier
+            );
+        } else {
+            var ghostlyVertexConsumerProvider = GhostlyVertexConsumer.Companion.convertToGhostlyVertexConsumerProvider(vertexConsumerProvider);
+            EquipmentRendererMixinExtension.INSTANCE.renderGhostly(
+                    equipmentRenderer,
+                    EquipmentModel.LayerType.WINGS,
+                    equippableComponent.assetId().get(),
+                    elytraEntityModel,
+                    itemStack,
+                    matrixStack,
+                    ghostlyVertexConsumerProvider,
+                    light,
+                    identifier
+            );
+        }
+    }
+}
--- src/client/java/de/fuballer/mcendgame/client/mixin/renderer/HeldItemFeatureRendererMixin.java
+++ src/client/java/de/fuballer/mcendgame/client/mixin/renderer/HeldItemFeatureRendererMixin.java
@@ -1,35 +1,27 @@
 package de.fuballer.mcendgame.client.mixin.renderer;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import de.fuballer.mcendgame.client.component.item.custom.armor.geistergaloschen.GhostlyVertexConsumer;
 import de.fuballer.mcendgame.client.util.EntityRenderStateMixinExtension;
-import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.HeldItemFeatureRenderer;
 import net.minecraft.client.render.entity.state.ArmedEntityRenderState;
-import net.minecraft.client.render.item.ItemRenderState;
-import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(HeldItemFeatureRenderer.class)
 public class HeldItemFeatureRendererMixin<S extends ArmedEntityRenderState> {
-    @Redirect(
+    @ModifyArg(
             method = "renderItem",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/ItemRenderState;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V")
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/ItemRenderState;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V"),
+            index = 1
     )
-    void render(
-            ItemRenderState itemState,
-            MatrixStack matrices,
+    VertexConsumerProvider render(
             VertexConsumerProvider vertexConsumers,
-            int light,
-            int overlay,
-            S entityState
+            @Local(argsOnly = true) S entityState
     ) {
-        if (!EntityRenderStateMixinExtension.INSTANCE.isGhostly(entityState)) itemState.render(matrices, vertexConsumers, light, OverlayTexture.DEFAULT_UV);
-        else {
-            var ghostlyVertexConsumerProvider = GhostlyVertexConsumer.Companion.convertToGhostlyVertexConsumerProvider(vertexConsumers);
-            itemState.render(matrices, ghostlyVertexConsumerProvider, light, OverlayTexture.DEFAULT_UV);
-        }
+        if (!EntityRenderStateMixinExtension.INSTANCE.isGhostly(entityState)) return vertexConsumers;
+        return GhostlyVertexConsumer.Companion.convertToGhostlyVertexConsumerProvider(vertexConsumers);
     }
 }
