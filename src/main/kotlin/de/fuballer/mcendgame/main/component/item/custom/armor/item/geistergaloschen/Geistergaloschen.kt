4b64676927b61626873b90b5251f6ea8edac801c
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2025-09-01 22:09:02 +0200

    remove backstab damage from geistergaloschen make geistergaloschen not drop

--- src/main/kotlin/de/fuballer/mcendgame/main/component/item/custom/armor/item/geistergaloschen/Geistergaloschen.kt
+++ src/main/kotlin/de/fuballer/mcendgame/main/component/item/custom/armor/item/geistergaloschen/Geistergaloschen.kt
@@ -1,9 +1,7 @@
 package de.fuballer.mcendgame.main.component.item.custom.armor.item.geistergaloschen
 
-import de.fuballer.mcendgame.main.component.custom_attribute.data.DoubleBounds
 import de.fuballer.mcendgame.main.component.custom_attribute.data.RollableCustomAttribute
 import de.fuballer.mcendgame.main.component.custom_attribute.types.CustomAttributeTypes
-import de.fuballer.mcendgame.main.component.custom_attribute.types.VanillaAttributeTypes
 import de.fuballer.mcendgame.main.component.item.custom.UniqueAttributesItem
 import net.minecraft.component.type.AttributeModifierSlot
 
@@ -14,8 +12,6 @@ class Geistergaloschen(
         RollableCustomAttribute(CustomAttributeTypes.GHOSTLY_APPEARANCE, 0),
         RollableCustomAttribute(CustomAttributeTypes.ENTITY_PHASING, 0),
         RollableCustomAttribute(CustomAttributeTypes.BLOCK_PHASING, 0),
-        RollableCustomAttribute(CustomAttributeTypes.MORE_BACKSTAB_DAMAGE, 0, DoubleBounds(0.25, 0.35)),
-        RollableCustomAttribute(VanillaAttributeTypes.INCREASED_MOVEMENT_SPEED, 0, DoubleBounds(0.05, 0.15)),
     )
 
     override fun getAttributeModifierSlot() = AttributeModifierSlot.FEET
