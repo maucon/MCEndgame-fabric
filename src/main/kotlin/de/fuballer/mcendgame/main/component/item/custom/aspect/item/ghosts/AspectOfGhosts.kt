f0061842e1733b63b0b1d065046377656217216a
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2025-09-04 22:01:26 +0200

    make aspect of ghost geistergaloschen text use item name

--- src/main/kotlin/de/fuballer/mcendgame/main/component/item/custom/aspect/item/ghosts/AspectOfGhosts.kt
+++ src/main/kotlin/de/fuballer/mcendgame/main/component/item/custom/aspect/item/ghosts/AspectOfGhosts.kt
@@ -1,5 +1,6 @@
 package de.fuballer.mcendgame.main.component.item.custom.aspect.item.ghosts
 
+import de.fuballer.mcendgame.main.component.item.custom.armor.CustomArmorItems
 import de.fuballer.mcendgame.main.component.item.custom.aspect.AspectItem
 import de.fuballer.mcendgame.main.component.item.custom.aspect.AspectItems
 import net.minecraft.text.Text
@@ -16,7 +17,7 @@ class AspectOfGhosts(
     override val description = mutableListOf(
         Text.translatable(TRANSLATABLE_DESCRIPTION_KEY + "ghosts_0", FORCED_DUNGEON_LEVEL),
         Text.translatable(TRANSLATABLE_DESCRIPTION_KEY + "ghosts_1"),
-        Text.translatable(TRANSLATABLE_DESCRIPTION_KEY + "ghosts_2")
+        Text.translatable(TRANSLATABLE_DESCRIPTION_KEY + "ghosts_2", CustomArmorItems.GEISTERGALOSCHEN.name)
     )
     override val disabledAspects = listOf(AspectItems.ASPECT_OF_IMPATIENCE)
 }
