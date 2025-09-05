decced613eb27a25a73195445c0b2496038d52d1
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2025-09-05 04:09:04 +0200

    make geckolib entities translucent when ghostly

--- src/client/java/de/fuballer/mcendgame/client/mixin/ghostly_appearance/GeoEntityRendererGhostlyMixin.java
+++ src/client/java/de/fuballer/mcendgame/client/mixin/ghostly_appearance/GeoEntityRendererGhostlyMixin.java
@@ -3,16 +3,21 @@ package de.fuballer.mcendgame.client.mixin.ghostly_appearance;
 import de.fuballer.mcendgame.client.accessor.LivingEntityRenderStateGhostlyAccessor;
 import de.fuballer.mcendgame.main.component.custom_attribute.CustomAttributesExtensions;
 import net.minecraft.client.item.ItemModelManager;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.entity.state.EntityRenderState;
 import net.minecraft.client.render.entity.state.LivingEntityRenderState;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.base.GeoRenderState;
 
 @Mixin(GeoEntityRenderer.class)
-public class GeoEntityRendererGhostlyMixin {
+public class GeoEntityRendererGhostlyMixin<R extends EntityRenderState & GeoRenderState> {
     @Inject(method = "extractLivingEntityRenderState", at = @At("HEAD"))
     void extractLivingEntityRenderState(
             LivingEntity entity,
@@ -25,5 +30,21 @@ public class GeoEntityRendererGhostlyMixin {
 
         var ghostly = CustomAttributesExtensions.INSTANCE.isGhostly(entity);
         accessor.mcendgame$setGhostly(ghostly);
+    }
+
+    @Inject(
+            method = "getRenderType(Lnet/minecraft/client/render/entity/state/EntityRenderState;Lnet/minecraft/util/Identifier;)Lnet/minecraft/client/render/RenderLayer;",
+            at = @At("HEAD"),
+            cancellable = true)
+    void getRenderType(
+            R renderState,
+            Identifier texture,
+            CallbackInfoReturnable<RenderLayer> cir
+    ) {
+        if (!(renderState instanceof LivingEntityRenderStateGhostlyAccessor accessor)) return;
+        if (!accessor.mcendgame$isGhostly()) return;
+
+        var renderLayer = RenderLayer.getEntityTranslucent(texture);
+        cir.setReturnValue(renderLayer);
     }
 }
