bb151ec41cded41613adb91a0a8c7c0cb4286c71
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2025-11-22 17:48:41 +0100

    add attributes to twinfire

--- src/main/kotlin/de/fuballer/mcendgame/main/component/item/custom/tool/item/Twinfire.kt
+++ src/main/kotlin/de/fuballer/mcendgame/main/component/item/custom/tool/item/Twinfire.kt
@@ -2,6 +2,7 @@ package de.fuballer.mcendgame.main.component.item.custom.tool.item
 
 import de.fuballer.mcendgame.main.component.custom_attribute.data.DoubleBounds
 import de.fuballer.mcendgame.main.component.custom_attribute.data.RollableCustomAttribute
+import de.fuballer.mcendgame.main.component.custom_attribute.types.CustomAttributeTypes
 import de.fuballer.mcendgame.main.component.custom_attribute.types.VanillaAttributeTypes
 import de.fuballer.mcendgame.main.component.item.custom.UniqueAttributesItem
 import net.minecraft.component.type.AttributeModifierSlot
@@ -10,8 +11,15 @@ class Twinfire(
     settings: Settings,
 ) : UniqueAttributesItem(settings) {
     override fun getCustomAttributes() = listOf(
-        RollableCustomAttribute(VanillaAttributeTypes.INCREASED_ATTACK_SPEED, 0, DoubleBounds(0.1, 0.2)),
+        getFlatDamageAttribute(),
+        RollableCustomAttribute(CustomAttributeTypes.DODGE, 0, DoubleBounds(0.05, 0.08)),
+        RollableCustomAttribute(CustomAttributeTypes.TWINFIRE_DUAL_WIELD_MORE_DAMAGE, 0, DoubleBounds(0.05, 0.1)),
     )
+
+    private fun getFlatDamageAttribute() = listOf(
+        RollableCustomAttribute(VanillaAttributeTypes.ATTACK_DAMAGE, 0, DoubleBounds(1.0, 2.0)),
+        RollableCustomAttribute(CustomAttributeTypes.ELEMENTAL_DAMAGE, 0, DoubleBounds(1.0, 2.0)),
+    ).random()
 
     override fun getAttributeModifierSlot() = AttributeModifierSlot.HAND
 }
