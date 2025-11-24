73ab4e7433c41449e4b822edaea3fb46afbdab5e
maucon <maucon13@gmail.com>
2025-11-24 22:19:43 +0100

    change repo directory

--- src/main/kotlin/de/fuballer/mcendgame/main/configuration/RuntimeConfig.kt
+++ src/main/kotlin/de/fuballer/mcendgame/main/configuration/RuntimeConfig.kt
@@ -1,15 +1,22 @@
 package de.fuballer.mcendgame.main.configuration
 
+import com.google.gson.JsonElement
+import com.mojang.serialization.JsonOps
 import de.maucon.mauconframework.di.annotation.Configuration
 import de.maucon.mauconframework.initializer.Initializer
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import net.minecraft.registry.RegistryOps
 import net.minecraft.server.MinecraftServer
+import net.minecraft.util.WorldSavePath
 import xyz.nucleoid.fantasy.Fantasy
+import java.nio.file.Path
 
 @Configuration
 object RuntimeConfig {
     lateinit var FANTASY: Fantasy
     lateinit var SERVER: MinecraftServer
+    lateinit var WORLD_SAVE_PATH: Path
+    lateinit var REGISTRY_OPS: RegistryOps<JsonElement>
 
     @Initializer
     fun init() {
@@ -17,6 +24,8 @@ object RuntimeConfig {
         ServerLifecycleEvents.SERVER_STARTING.register { server ->
             this.SERVER = server
             this.FANTASY = Fantasy.get(server)
+            this.WORLD_SAVE_PATH = server.getSavePath(WorldSavePath.ROOT)
+            this.REGISTRY_OPS = RegistryOps.of(JsonOps.INSTANCE, server.registryManager)
         }
     }
 }
