f7ca7da597db15aaaded007a7f157d87b4189e40
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2025-11-30 20:35:37 +0100

    magic numbers

--- src/main/kotlin/de/fuballer/mcendgame/main/component/status_effect/verdant_echo/VerdantEchoEffectService.kt
+++ src/main/kotlin/de/fuballer/mcendgame/main/component/status_effect/verdant_echo/VerdantEchoEffectService.kt
@@ -5,11 +5,14 @@ import de.fuballer.mcendgame.main.component.status_effect.CustomStatusEffects
 import de.maucon.mauconframework.command.CommandHandler
 import de.maucon.mauconframework.di.annotation.Injectable
 
+private const val BASE_MORE_DAMAGE_TAKEN = -0.1
+private const val MORE_DAMAGE_TAKEN_PER_AMPLIFIER = -0.05
+
 @Injectable
 class VerdantEchoEffectService {
     @CommandHandler
     fun on(cmd: DamageCalculationCommand) {
         val amplifier = cmd.damaged.getStatusEffect(CustomStatusEffects.VERDANT_ECHO)?.amplifier ?: return
-        cmd.moreDamageTaken.add(-0.1 + (amplifier * -0.05))
+        cmd.moreDamageTaken.add(BASE_MORE_DAMAGE_TAKEN + (amplifier * MORE_DAMAGE_TAKEN_PER_AMPLIFIER))
     }
 }
