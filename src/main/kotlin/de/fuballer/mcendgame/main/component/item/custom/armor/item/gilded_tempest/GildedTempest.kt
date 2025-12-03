138453a80174a76301d24724521ae8f7220b1973
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2025-12-03 21:49:37 +0100

    adjust gildet tempest stats

--- src/main/kotlin/de/fuballer/mcendgame/main/component/item/custom/armor/item/gilded_tempest/GildedTempest.kt
+++ src/main/kotlin/de/fuballer/mcendgame/main/component/item/custom/armor/item/gilded_tempest/GildedTempest.kt
@@ -13,8 +13,8 @@ class GildedTempest(
     settings: Settings,
 ) : UniqueAttributesItem(settings), HidePlayerModelPartArmor {
     override fun getCustomAttributes() = listOf(
-        RollableCustomAttribute(CustomAttributeTypes.MOVEMENT_SPEED_MODIFIERS_AFFECT_DAMAGE, 0, DoubleBounds(0.15, 0.25)),
-        RollableCustomAttribute(VanillaAttributeTypes.INCREASED_MOVEMENT_SPEED, 0, DoubleBounds(0.1, 0.2)),
+        RollableCustomAttribute(CustomAttributeTypes.MOVEMENT_SPEED_MODIFIERS_AFFECT_DAMAGE, 0, DoubleBounds(0.2, 0.3)),
+        RollableCustomAttribute(VanillaAttributeTypes.INCREASED_MOVEMENT_SPEED, 0, DoubleBounds(0.1, 0.15)),
     )
 
     override fun getAttributeModifierSlot() = AttributeModifierSlot.LEGS
