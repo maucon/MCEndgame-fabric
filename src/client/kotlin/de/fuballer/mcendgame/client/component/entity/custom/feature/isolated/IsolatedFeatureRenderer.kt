1daae39abdde439d3864d5da1611be16b3de5553
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2026-02-26 21:20:37 +0100

    isolated rendering

--- src/client/kotlin/de/fuballer/mcendgame/client/component/entity/custom/feature/isolated/IsolatedFeatureRenderer.kt
+++ src/client/kotlin/de/fuballer/mcendgame/client/component/entity/custom/feature/isolated/IsolatedFeatureRenderer.kt
@@ -1,6 +1,6 @@
 package de.fuballer.mcendgame.client.component.entity.custom.feature.isolated
 
-import net.minecraft.client.render.VertexConsumerProvider
+import net.minecraft.client.render.command.OrderedRenderCommandQueue
 import net.minecraft.client.render.entity.feature.FeatureRenderer
 import net.minecraft.client.render.entity.feature.FeatureRendererContext
 import net.minecraft.client.render.entity.model.EntityModel
@@ -11,15 +11,14 @@ import net.minecraft.client.util.math.MatrixStack
 class IsolatedFeatureRenderer<T : LivingEntityRenderState, M : EntityModel<T>>(
     featureContext: FeatureRendererContext<T, M>,
 ) : FeatureRenderer<T, M>(featureContext) {
-
     override fun render(
         matrices: MatrixStack,
-        vertexConsumers: VertexConsumerProvider,
+        queue: OrderedRenderCommandQueue,
         light: Int,
         state: T,
         limbAngle: Float,
-        limbDistance: Float
+        limbDistance: Float,
     ) {
-        IsolatedIndicatorRenderer.tryRender(state, matrices, vertexConsumers, light)
+        IsolatedIndicatorRenderer.tryRender(state, matrices, queue, light)
     }
 }
