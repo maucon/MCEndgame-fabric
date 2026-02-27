669e0926f75807c19380606eafca2eada3f9c1cb
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2026-02-27 02:31:56 +0100

    ghostly

--- src/client/kotlin/de/fuballer/mcendgame/client/component/render/CustomRenderLayers.kt
+++ src/client/kotlin/de/fuballer/mcendgame/client/component/render/CustomRenderLayers.kt
@@ -1,7 +1,12 @@
 package de.fuballer.mcendgame.client.component.render
 
+import net.minecraft.client.gl.RenderPipelines
+import net.minecraft.client.render.OutputTarget
 import net.minecraft.client.render.RenderLayer
 import net.minecraft.client.render.RenderSetup
+import net.minecraft.util.Identifier
+import net.minecraft.util.Util
+import java.util.function.Function
 
 object CustomRenderLayers {
     val LINK: RenderLayer = RenderLayer.of(
@@ -10,4 +15,18 @@ object CustomRenderLayers {
             .useLightmap()
             .build()
     )
+
+    fun ghostly(texture: Identifier) = GHOSTLY.apply(texture)
+    val GHOSTLY: Function<Identifier, RenderLayer> = Util.memoize<Identifier, RenderLayer> { texture ->
+        val renderSetup = RenderSetup.builder(RenderPipelines.RENDERTYPE_ITEM_ENTITY_TRANSLUCENT_CULL)
+            .texture("Sampler0", texture)
+            .outputTarget(OutputTarget.ITEM_ENTITY_TARGET)
+            .useLightmap()
+            .useOverlay()
+            .crumbling()
+            .translucent()
+            .outlineMode(RenderSetup.OutlineMode.AFFECTS_OUTLINE)
+            .build()
+        RenderLayer.of("ghostly", renderSetup)
+    }
 }
