7b6751d8d0b3f8d3165d57e5d46d2e9191b2235c
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2026-03-05 00:10:43 +0100

    fix block phasing fog rendering

--- src/client/java/de/fuballer/mcendgame/client/mixin/phasing/BackgroundRendererBlockPhasingMixin.java
+++ src/client/java/de/fuballer/mcendgame/client/mixin/phasing/BackgroundRendererBlockPhasingMixin.java
@@ -3,20 +3,25 @@ package de.fuballer.mcendgame.client.mixin.phasing;
 import com.llamalad7.mixinextras.sugar.Local;
 import de.fuballer.mcendgame.main.util.extension.EntityExtension;
 import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.RenderTickCounter;
-import net.minecraft.client.render.fog.FogData;
 import net.minecraft.client.render.fog.FogRenderer;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.nio.ByteBuffer;
+
 @Mixin(FogRenderer.class)
-public class BackgroundRendererBlockPhasingMixin {
+public abstract class BackgroundRendererBlockPhasingMixin {
+    @Shadow
+    protected abstract void applyFog(ByteBuffer buffer, int bufPos, Vector4f fogColor, float environmentalStart, float environmentalEnd, float renderDistanceStart, float renderDistanceEnd, float skyEnd, float cloudEnd);
+
     @Unique
     private static final Vector4f PHASING_FOG_COLOR = new Vector4f(0.2F, 0.7F, 0.6F, 1F);
 
@@ -38,28 +43,24 @@ public class BackgroundRendererBlockPhasingMixin {
         cir.setReturnValue(PHASING_FOG_COLOR);
     }
 
-    @Inject(
+    @Redirect(
             method = "applyFog(Lnet/minecraft/client/render/Camera;ILnet/minecraft/client/render/RenderTickCounter;FLnet/minecraft/client/world/ClientWorld;)Lorg/joml/Vector4f;",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/render/fog/FogRenderer;applyFog(Ljava/nio/ByteBuffer;ILorg/joml/Vector4f;FFFFFF)V"
-            )
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/fog/FogRenderer;applyFog(Ljava/nio/ByteBuffer;ILorg/joml/Vector4f;FFFFFF)V")
     )
-    private void modifyFogData(
-            Camera camera,
-            int viewDistance,
-            RenderTickCounter tickCounter,
-            float skyDarkness,
-            ClientWorld world,
-            CallbackInfoReturnable<Vector4f> cir,
-            @Local FogData fogData
+    void a(
+            FogRenderer instance,
+            ByteBuffer buffer,
+            int bufPos,
+            Vector4f fogColor,
+            float environmentalStart,
+            float environmentalEnd,
+            float renderDistanceStart,
+            float renderDistanceEnd,
+            float skyEnd,
+            float cloudEnd,
+            @Local Entity entity
     ) {
-        Entity entity = camera.getFocusedEntity();
         if (entity == null || !EntityExtension.INSTANCE.isPhasingThroughWall(entity)) return;
-
-        fogData.environmentalStart = 0F;
-        fogData.environmentalEnd = 4F;
-        fogData.renderDistanceStart = 0F;
-        fogData.renderDistanceEnd = 4F;
+        applyFog(buffer, bufPos, fogColor, 0F, 4F, 0F, 4F, 0F, 4F);
     }
 }
