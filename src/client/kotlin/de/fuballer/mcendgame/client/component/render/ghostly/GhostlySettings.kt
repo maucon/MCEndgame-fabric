a2d9f5aab141e2fc4c347521553ce43fe58ca677
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2026-03-25 18:51:39 +0100

    Ghostly Feature Render (#207)
    * render bogged&stray overlay ghostly (everything that uses FeatureRenderers methods)
    
    * ghostly settings

---
+++ src/client/kotlin/de/fuballer/mcendgame/client/component/render/ghostly/GhostlySettings.kt
@@ -0,0 +1,7 @@
+package de.fuballer.mcendgame.client.component.render.ghostly
+
+import de.fuballer.mcendgame.main.util.ColorUtil
+
+object GhostlySettings {
+    val COLOR = ColorUtil.rgbaToInt(10, 235, 200, 120)
+}
