f1c00c137c5ccdcd076fa7c302e57503c3106470
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2026-04-28 20:29:01 +0200

    Update Additional Arrows Implementation (#230)
    * completely change additional arrows implementation
    (now uses minecrafts system via mixin)
    
    * prevent negative additional arrow count
    
    * kotlin version

--- src/main/kotlin/de/fuballer/mcendgame/main/util/extension/mixin/PersistentProjectileEntityMixinExtension.kt
+++ src/main/kotlin/de/fuballer/mcendgame/main/util/extension/mixin/PersistentProjectileEntityMixinExtension.kt
@@ -1,19 +1,10 @@
 package de.fuballer.mcendgame.main.util.extension.mixin
 
 import de.fuballer.mcendgame.main.accessor.PersistentProjectileEntityDamageAccessor
-import de.fuballer.mcendgame.main.accessor.PersistentProjectileEntityIsAdditionalAccessor
 import de.fuballer.mcendgame.main.accessor.PersistentProjectileEntityPierceLevelAccessor
 import net.minecraft.entity.projectile.PersistentProjectileEntity
 
 object PersistentProjectileEntityMixinExtension {
-    fun PersistentProjectileEntity.setLoadProcessed(processed: Boolean = true) = (this as PersistentProjectileEntityIsAdditionalAccessor).`mcendgame$setLoadProcessed`(processed)
-
-    fun PersistentProjectileEntity.hasLoadBeenProcessed() = (this as PersistentProjectileEntityIsAdditionalAccessor).`mcendgame$hasLoadBeenProcessed`()
-
-    fun PersistentProjectileEntity.setIsAdditional(isAdditional: Boolean = true) = (this as PersistentProjectileEntityIsAdditionalAccessor).`mcendgame$setIsAdditional`(isAdditional)
-
-    fun PersistentProjectileEntity.isAdditional() = (this as PersistentProjectileEntityIsAdditionalAccessor).`mcendgame$isAdditional`()
-
     fun PersistentProjectileEntity.setPierceLevel(level: Byte) = (this as PersistentProjectileEntityPierceLevelAccessor).`mcendgame$callSetPierceLevel`(level)
 
     fun PersistentProjectileEntity.getDamage() = (this as PersistentProjectileEntityDamageAccessor).`mcendgame$getDamage`()
