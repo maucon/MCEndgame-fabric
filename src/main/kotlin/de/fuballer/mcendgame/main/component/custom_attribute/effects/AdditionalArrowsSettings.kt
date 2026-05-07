4cf028d311f57df1ecd58f9dfd66971bd54a039d
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2026-05-07 16:40:21 +0200

    make skeletons work with additional arrows again

---
+++ src/main/kotlin/de/fuballer/mcendgame/main/component/custom_attribute/effects/AdditionalArrowsSettings.kt
@@ -0,0 +1,5 @@
+package de.fuballer.mcendgame.main.component.custom_attribute.effects
+
+object AdditionalArrowsSettings {
+    const val SPREAD_PER_ARROW = 5F // the resulting cone has an angle of this times two
+}
